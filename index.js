b7a10955db1ce93a79bb6669b8c0e6b1120aef1b
abdallahark <abdullahragab95285@gmail.com>
2025-10-22 19:42:33 +0300

    <commit-message>

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app= express()
+const port=3000
+app.set('view engine', 'ejs')
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+app.listen(port,()=>{
+    console.log('listing on port 3000')
+})
